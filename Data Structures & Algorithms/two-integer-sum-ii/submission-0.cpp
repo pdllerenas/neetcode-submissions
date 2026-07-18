c16e59d0d5db456bac2bab1c0943edefa176f010
pdllerenas <134813317+pdllerenas@users.noreply.github.com>
2026-07-18 10:11:32 -0600

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = (int)numbers.size() - 1;
+        int prev = numbers[r];
+        while (l < r) {
+            int diff = target - numbers[r];
+            cout << diff << endl;
+            if (numbers[l] == diff) {
+                return {l + 1, r + 1};
+            } else if (numbers[l] > diff) {
+                while (numbers[r] == numbers[r-1]) {
+                    r--;
+                }
+                r--;
+            } else {
+                while (numbers[l] == numbers[l+1]) {
+                    l++;
+                }
+                l++;
+            }
+        }
+        return {};
+    }
+};
