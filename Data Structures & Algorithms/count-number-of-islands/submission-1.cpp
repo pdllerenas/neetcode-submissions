b3a9148c8d1a88e93d9928c07536631160b83e1f
pdllerenas <134813317+pdllerenas@users.noreply.github.com>
2026-07-31 17:03:30 -0600

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+ private:
+  void dfs(vector<vector<char>>& grid, int i, int j) {
+    int n = grid.size();
+    int m = grid[0].size();
+    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') return;
+    grid[i][j] = '0';
+    dfs(grid, i-1, j);
+    dfs(grid, i+1, j);
+    dfs(grid, i, j-1);
+    dfs(grid, i, j+1);
+  }
+
+ public:
+  int numIslands(vector<vector<char>>& grid) {
+    int n = grid.size();
+    int m = grid[0].size();
+    int res = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (grid[i][j] == '1') {
+                res++;
+                dfs(grid, i, j);
+            }
+        }
+    }
+    return res;
+  }
+};
